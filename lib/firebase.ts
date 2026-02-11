3f20c5560ba057a68e3236aeacd05283b6f219c9
BRNCAR043 <BRNCAR043@myuct.ac.za>
2026-02-11 16:46:17 +0200

    connected to firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_snLVtQfhaHhbiyGnH-9Meqjq6npQC0c",
+  authDomain: "vinyl-store-5dacb.firebaseapp.com",
+  projectId: "vinyl-store-5dacb",
+  storageBucket: "vinyl-store-5dacb.firebasestorage.app",
+  messagingSenderId: "309226868343",
+  appId: "1:309226868343:web:b17df477fc93dfb1ff0072"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
